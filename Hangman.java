0745ee748a553838446a27e4938f1cdb17ad721e
chloetohce <chloetohce@gmail.com>
2024-10-06 00:04:38 +0800

    first commit

---
+++ Hangman.java
@@ -0,0 +1,11 @@
+public class Hangman {
+    /* TASK
+     * 1. Create a word list file
+     * 2. Read the word list file in an array of words
+     * 3. Pick a word randomly from the array of words
+     * 4. Guess the word
+     * 5. Prompt to enter a character
+     * 6. If character exists in the word, display it as _ _ _ o _ _
+     * 7. Repeat until the word is complete
+     */
+}
